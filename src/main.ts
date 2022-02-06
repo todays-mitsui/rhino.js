0049cdb6e25622664c0beb49a657e6687dc8a179
三井翔吾 <mitsui@atsu-maru.co.jp>
2022-02-06 17:20:00 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { texture } from './texture';
 import './style.css';
 
 const devicemotion = new DeviceMotion();
-
 const start = document.querySelector<HTMLDivElement>('.start')!;
 start.addEventListener('click', async () => {
   const state = await devicemotion.requestPermission();
@@ -17,55 +16,59 @@ start.addEventListener('click', async () => {
       devicemotion.hander(event);
     });
   }
-});
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-const cw = app.clientWidth;
-const ch = app.clientHeight;
-
-const dice = createDice(cw / 2, ch / 4);
-
-/* @ts-ignore */
-const { engine, render, runner, ground } = rhinoCanvas(app, dice);
-
-app.addEventListener('click', () => {
-  setVelocity(dice);
-});
-
-const textureMap = new Map([
-  [1, texture(1)],
-  [2, texture(2)],
-  [3, texture(3)],
-  [4, texture(4)],
-  [5, texture(5)],
-  [6, texture(6)],
-]);
-
-Events.on(engine, 'beforeUpdate', changeTexture(dice, textureMap));
-
-document.querySelector<HTMLButtonElement>('#fill')!
-  .addEventListener('click', () => {
-    ground.render!.fillStyle = '#000000';
-    ground.render!.lineWidth = 0;
-    ground.render!.strokeStyle = 'transparent';
+  devicemotion.onDeviceMotion(() => {
+    if (devicemotion.enable) {
+      console.log({ acceleration: devicemotion.acceleration });
+    }
   });
 
-document.querySelector<HTMLButtonElement>('#line')!
-  .addEventListener('click', () => {
-    ground.render!.fillStyle = 'transparent';
-    ground.render!.lineWidth = 0.5;
-    ground.render!.strokeStyle = '#000000';
-  });
-
-document.querySelector<HTMLButtonElement>('#transparent')!
-  .addEventListener('click', () => {
-    ground.render!.fillStyle = 'transparent';
-    ground.render!.lineWidth = 0;
-    ground.render!.strokeStyle = 'transparent';
-  });
-
-devicemotion.onDeviceMotion(() => {
-  if (devicemotion.enable) {
-    console.log({ acceleration: devicemotion.acceleration });
-  }
+  main();
 });
+
+function main() {
+  const app = document.querySelector<HTMLDivElement>('#app')!;
+  const cw = app.clientWidth;
+  const ch = app.clientHeight;
+
+  const dice = createDice(cw / 2, ch / 4);
+
+  /* @ts-ignore */
+  const { engine, render, runner, ground } = rhinoCanvas(app, dice);
+
+  app.addEventListener('click', () => {
+    setVelocity(dice);
+  });
+
+  const textureMap = new Map([
+    [1, texture(1)],
+    [2, texture(2)],
+    [3, texture(3)],
+    [4, texture(4)],
+    [5, texture(5)],
+    [6, texture(6)],
+  ]);
+
+  Events.on(engine, 'beforeUpdate', changeTexture(dice, textureMap));
+
+  document.querySelector<HTMLButtonElement>('#fill')!
+    .addEventListener('click', () => {
+      ground.render!.fillStyle = '#000000';
+      ground.render!.lineWidth = 0;
+      ground.render!.strokeStyle = 'transparent';
+    });
+
+  document.querySelector<HTMLButtonElement>('#line')!
+    .addEventListener('click', () => {
+      ground.render!.fillStyle = 'transparent';
+      ground.render!.lineWidth = 0.5;
+      ground.render!.strokeStyle = '#000000';
+    });
+
+  document.querySelector<HTMLButtonElement>('#transparent')!
+    .addEventListener('click', () => {
+      ground.render!.fillStyle = 'transparent';
+      ground.render!.lineWidth = 0;
+      ground.render!.strokeStyle = 'transparent';
+    });
+}
