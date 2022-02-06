513cd7d1f3a40dcb08dd4c260a552888a013da9d
三井翔吾 <mitsui@atsu-maru.co.jp>
2022-02-06 21:14:07 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -72,7 +72,7 @@ function main(devicemotion: DeviceMotion) {
     if (devicemotion.enable) {
       const { norm } = devicemotion.acceleration;
       if (norm > 1) {
-        onAcceleration(norm);
+        onAcceleration(norm * 0.2);
       }
     }
   });
