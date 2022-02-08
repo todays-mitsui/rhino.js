e37a5022638166288dda4ef267199165e9c92f9e
三井翔吾 <mitsui@atsu-maru.co.jp>
2022-02-09 01:44:32 +0900

    .

--- src/body/dice.ts
+++ src/body/dice.ts
@@ -2,6 +2,7 @@ import { Bodies, Body } from 'matter-js';
 import { nextPips, Pips } from '../physics';
 import { nextVelocity } from '../physics';
 // import { texture } from './texture';
+import img from '../img/dice-6.png';
 
 export function createDice(x: number, y: number, size: number = 30): Body {
   const dice = Bodies.rectangle(
@@ -12,7 +13,7 @@ export function createDice(x: number, y: number, size: number = 30): Body {
       friction: 0.05,     // 摩擦係数
       render: {
         sprite: {
-          texture: '/img/dice-6.png',
+          texture: img,
           xScale: 0.206897,
           yScale: 0.206897,
         },
